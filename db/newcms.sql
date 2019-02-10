333423c55d3d2ee45d2689937379b7cd56918475
Pruthvi Patel <pruthvipatel145@gmail.com>
2019-02-10 17:20:35 +0530

    Update newcms.sql

--- db/newcms.sql
+++ db/newcms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2019 at 05:49 AM
+-- Generation Time: Feb 10, 2019 at 12:49 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -46,7 +46,8 @@ INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
 (59, 'c#'),
 (60, 'node'),
 (66, 'new ha'),
-(67, 'new ha nice');
+(67, 'new ha nice'),
+(68, 'new ha');
 
 -- --------------------------------------------------------
 
@@ -69,7 +70,9 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`comment_id`, `comment_post_id`, `comment_author`, `comment_email`, `comment_content`, `comment_status`, `comment_date`) VALUES
-(41, 362, 'dcvsdvdsv', 'vdsvdvsv', 'dsvdsvdsvsv', 'approved', '0000-00-00');
+(41, 362, 'dcvsdvdsv', 'vdsvdvsv', 'dsvdsvdsvsv', 'unapproved', '0000-00-00'),
+(45, 370, 'vdsvsdvs', 'vsdvsdvsdv', 'dsvsdvsdvsvdsv', 'approved', '0000-00-00'),
+(46, 370, 'dsvsvsdvsdv', 'dsvsvsdvsv', 'vdsvvsvsvsdvdsv', 'approved', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -95,9 +98,61 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`post_id`, `post_cat_id`, `post_title`, `post_author`, `post_date`, `post_image`, `post_content`, `post_tags`, `post_comment_count`, `post_status`) VALUES
-(359, 56, 'java demo post', 'Pruthvi Patel', '0000-00-00', 'Screenshot (1).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
 (362, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 1, 'published'),
-(369, 58, 'Demo post ', 'Pruthvi Patel', '0000-00-00', 'myAvatar.png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published');
+(370, 56, 'java demo post', 'Pruthvi Patel', '0000-00-00', 'Screenshot (1).png', 'there are huge content of lorem ipsum', 'new tag', 2, 'published'),
+(371, 56, 'java demo post', 'Pruthvi Patel', '0000-00-00', 'Screenshot (1).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(372, 58, 'Demo post ', 'Pruthvi Patel', '0000-00-00', 'myAvatar.png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(373, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(374, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(375, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(376, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(377, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(378, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(379, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(380, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(381, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(382, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(383, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(384, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(385, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(386, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(387, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(388, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(389, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(390, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(391, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(392, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(393, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(394, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published'),
+(395, 55, 'new post bitch', 'Pruthvi Patel', '0000-00-00', 'Screenshot (13).png', 'there are huge content of lorem ipsum', 'new tag', 0, 'published');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_firstname` varchar(255) NOT NULL,
+  `user_lastname` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_image` text NOT NULL,
+  `user_role` varchar(255) NOT NULL,
+  `randSalt` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_firstname`, `user_lastname`, `user_email`, `user_image`, `user_role`, `randSalt`) VALUES
+(25, 'pruthvipatel', 'test', 'Pruthvi', 'Patel', 'pruthvi200@gmail.com', 'Screenshot (18).png', 'admin', 'passenc'),
+(26, 'test', 'test', 'john', 'doe', 'john@yahho.com', 'Screenshot (25).png', 'subscriber', 'passenc'),
+(27, 'test2', 'test2', 'john', 'smith', 'johns@yahho.com', 'Screenshot (1).png', 'admin', 'passenc'),
+(28, 'test2', 'test2', 'john', 'smith', 'johns@yahho.com', 'Screenshot (1).png', 'subscriber', 'passenc');
 
 --
 -- Indexes for dumped tables
@@ -122,6 +177,12 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`post_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -129,19 +190,25 @@ ALTER TABLE `posts`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `cat_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `cat_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=370;
+  MODIFY `post_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=396;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
